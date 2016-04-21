c577568aee96d424b9db827a57d61875fbec5d0d
LincolnColeHoward <lincoln.c.howard.jr@gmail.com>
2016-04-21 06:18:50 -0400

    slight changes to index.js and index.html

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 var express = require ("express");
 var app = express ();
 app.use (require ("compression") ());
-app.use (express.static ("assets"));
+app.use (express.static ("assets", {
+	maxage: "172800"
+}));
 app.get ("/", function (req, res) {
 	res.redirect ("/index.html");
 });
