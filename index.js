167f046c9d4786872da6f10e37ec7ed438fe936a
LincolnColeHoward <lincoln.c.howard.jr@gmail.com>
2016-04-21 10:35:34 -0400

    get rid of bootstrap stuff we don't need!

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require ("express");
 var app = express ();
 app.use (require ("compression") ());
 app.use (express.static ("assets", {
-	maxage: "172800"
+	maxage: "172800000"
 }));
 app.get ("/", function (req, res) {
 	res.redirect ("/index.html");
