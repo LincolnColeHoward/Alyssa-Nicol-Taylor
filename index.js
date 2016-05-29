f2f1494042ba174b0066a470df3922784a2cf417
LincolnColeHoward <lincoln.c.howard.jr@gmail.com>
2016-05-28 20:36:24 -0400

    use https

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // express
+var https = require ("https");
+var fs = require ("fs");
 var express = require ("express");
 var app = express ();
 app.use (require ("compression") ());
-
 
 app.use (express.static ("assets/other"));
 app.get ("/", function (req, res) {
@@ -24,4 +25,15 @@ app.use (require ("./data/images"));
 app.use (require ("./data/graphics"));
 app.use (require ("./data/contact"));
 
-app.listen (80);
+var forward = express ();
+forward.all ("*", function (req, res) {
+	res.redirect ("https://antaylorco.com" + req.url);
+});
+
+forward.listen (80);
+
+var server = https.createServer ({
+	key: fs.readFileSync ("./ssl/cert.key"),
+	cert: fs.readFileSync ("./ssl/antaylorco.com/OtherServer/2_antaylorco.com.crt")
+}, app);
+server.listen (443);
